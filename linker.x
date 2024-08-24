19e7d3710c79bfb37016d327a7c8d6742a3d370a
Dan Nasman <dan.nasman@gmail.com>
2024-08-24 13:05:59 +0300

    add EL handling

--- linker.x
+++ linker.x
@@ -2,38 +2,13 @@ ENTRY(_start)
 SECTIONS
 {
     . = 0x40080000;
-    __start = .;
-    __text_start = .;
-    .text : {
-        KEEP(*(.text.boot))
-        *(.text) 
-    }
-    . = ALIGN(4096);
-    __text_end = .;
+    .text.boot : { *(.text.boot) }
+    .text : { *(.text) }
+    .data : { *(.data) }
+    .rodata : { *(.rodata) }
+    .bss : { *(.bss) }
 
-    __rodata_start = .;
-    .rodata : {
-        *(.rodata)
-    }
-    . = ALIGN(4096);
-    __rodata_end = .;
-
-    __data_start = .;
-    .data :
-    {
-        *(.data)
-    }
-    . = ALIGN(4096);
-    __data_end = .;
-
-    __bss_start = .;
-    .bss :
-    {
-        bss = .;
-        *(.bss)
-    }
-    . = ALIGN(4096);
-    __bss_end = .;
-    __bss_size = __bss_end - __bss_start;
-    __end = .;
+    . = ALIGN(8);
+    . = . + 0x4000;
+    __stack_top = .;
 }
--- src/boot.s
+++ src/boot.s
@@ -1,11 +1,47 @@
 .section ".text.boot"
+.extern __stack_top
 
 .globl _start
 
 _start:
-    ldr     x30, =_start
+    mrs     x0, CurrentEL
+    cmp     x0, 0b0100
+    beq     in_el1
+    blo     in_el0
+    cmp     x0, 0b1000
+    beq     in_el2
+in_el3:
+    msr     sctlr_el2, xzr
+    msr     hcr_el2, xzr
+
+    mrs     x0, scr_el3
+    orr     x0, x0, (1 << 10)
+    orr     x0, x0, (1 << 0)
+    msr     scr_el3, x0
+    mov     x0, 0b01001
+    msr     spsr_el3, x0
+
+    adr     x0, in_el2
+    msr     ELR_EL3, x0
+    eret
+in_el2:
+    msr     sctlr_el1, xzr
+    mrs     x0, HCR_EL2
+    orr     x0, x0, (1 << 31)
+    msr     hcr_el2, x0
+    mov     x0, 0b00101
+    msr     spsr_el2, x0
+
+    adr     x0, in_el1
+    msr     elr_el2, x0
+    eret
+in_el1:
+    ldr     x30, =__stack_top
     mov     sp, x30
+
     bl      kernel_main
+in_el0:
+    b .
 
 .equ PSCI_SYSTEM_OFF, 0x84000008
 .globl system_off
