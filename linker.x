3e0469dadcdc7117c5a0ea2a7af4ae78093c8ec2
Dan Nasman <dan.nasman@gmail.com>
2024-09-02 21:25:55 +0300

    mmu rework

--- linker.x
+++ linker.x
@@ -1,14 +1,63 @@
+__ram_start = 0x40080000;
+__kernel_start = __ram_start + 0x200000;
+
+__physical_stack = __kernel_start + 16M;
+__virtual_stack = 0xffff000000000000;
+
+__stack_len = 2M;
+__kernel_code_virtual = 0xffff000000400000;
+
+__kernel_virtual_ram_start = __kernel_code_virtual + (__kernel_start - __ram_start);
+
 ENTRY(_start)
 SECTIONS
 {
-    . = 0x40080000;
-    .text.boot : { *(.text.boot) }
-    .text : { *(.text) }
-    .data : { *(.data) }
-    .rodata : { *(.rodata) }
-    .bss : { *(.bss) }
+    . = __kernel_start;
 
-    . = ALIGN(8);
-    . = . + 0x1000;
-    __stack_top = .;
+    .physical_boot.text : {
+        __boot_start = .;
+        *(.init)
+        __boot_end = ABSOLUTE(.);
+    }
+
+    . = __virtual_stack;
+
+    .stack (NOLOAD) : AT(__physical_stack)
+    {
+        __estack = .;
+        . += __stack_len;
+        __sstack = .;
+    }
+
+    __kernel_virtual_code_start = __kernel_virtual_ram_start + (__boot_end - __kernel_start);
+    HIDDEN(__kernel_va_code_offset = __kernel_virtual_code_start - __boot_end);
+    . = __kernel_virtual_code_start;
+
+    .text : AT(ADDR(.text) - __kernel_va_code_offset) {
+        *(.text.abort);
+        *(text .text.*)
+    }
+
+    .rodata ALIGN(4) : AT(ADDR(.rodata) - __kernel_va_code_offset) {
+        *(.srodata .srodata.*);
+        *(.rodata .rodata.*);
+        . = ALIGN(4);
+    }
+
+    .data ALIGN(8) : AT(ADDR(.data) - __kernel_va_code_offset) {
+        __sidata = LOADADDR(.data);
+        __sdata = .;
+        PROVIDE(__global_pointer$ = . + 0x800);
+        *(.sdata .sdata.* .sdata2 .sdata2.*);
+        *(.data .data.*);
+        . = ALIGN(8);
+        __edata = .;
+    }
+
+    .bss ALIGN(8) (NOLOAD) : AT(ADDR(.data) - __kernel_va_code_offset) {
+        __sbss = .;
+        *(.sbss .sbss.* .bss .bss.*);
+        . = ALIGN(8);
+        __ebss = .;
+    }
 }
--- src/boot.s
+++ src/boot.s
@@ -1,12 +1,8 @@
-.section ".text.boot"
-.extern __stack_top
-
+.section ".init"
 .globl _start
+.extern __physical_stack
 
 _start:
-    ldr     x30, =__stack_top
-    mov     sp, x30
-
     mrs     x0, CurrentEL
     cmp     x0, 0b0100
     beq     in_el1
@@ -56,9 +52,93 @@ in_el1:
 
     // TOOD: msr     SP_EL1, x30 (causes exception)
 
-    bl      kernel_main
+    ldr     x0, =page_table_l0
+    ldr     x1, =page_table_l1
+    ldr     x2, =page_table_l2
+    ldr     x3, =page_table_l3
+
+    mov     x4, 0
+    stp     x4, x4, [x0]
+    stp     x4, x4, [x1]
+    stp     x4, x4, [x2]
+    stp     x4, x4, [x3]
+
+    ldr     x5, =page_table_l1
+    orr     x5, x5, 0x3
+    str     x5, [x0]
+
+    ldr     x5, =page_table_l2
+    orr     x5, x5, 0x3
+    str     x5, [x1]
+
+    ldr     x5, =page_table_l3
+    orr     x5, x5, 0x3
+    str     x5, [x2]
+
+    ldr     x6, =__ram_start
+    ldr     x7, =__kernel_code_virtual
+    orr     x6, x6, 0x3
+    lsr     x7, x7, 12
+    and     x7, x7, 0x1ff
+    str     x6, [x3, x7, LSL 3]
+
+    ldr     x6, =__physical_stack
+    ldr     x7, =__virtual_stack
+    orr     x6, x6, 0x3
+    lsr     x7, x7, 12
+    and     x7, x7, 0x1ff
+    str     x6, [x3, x7, LSL 3]
+
+    ldr     x0, =page_table_l0
+    msr     ttbr1_el1, x0
+    isb
+
+    ldr     x0, =0xFF000004FF
+    msr     mair_el1, x0
+
+    mov     x0, (16 << 0)
+    orr     x0, x0, (1 << 30)
+    orr     x0, x0, (3 << 28)
+    orr     x0, x0, (1 << 26)
+    orr     x0, x0, (1 << 24)
+    mov     x1, (16 << 16)
+    orr     x0, x0, x1
+    //orr     x0, x0, (1 << 14)
+    orr     x0, x0, (3 << 12)
+    orr     x0, x0, (1 << 10)
+    orr     x0, x0, (1 << 8)
+    msr     tcr_el1, x0
+    isb
+
+    mrs     x0, sctlr_el1
+    orr     x0, x0, 1
+    msr     sctlr_el1, x0
+    isb
+
+    ldr     x30, =__sstack
+    mov     sp, x30
+
+    ldr     x0, =kernel_main
+    blr     x0
+
 in_el0:
     b .
+
+.align 12
+page_table_l0:
+    .skip 4096, 0
+
+.align 12
+page_table_l1:
+    .skip 4096, 0
+
+.align 12
+page_table_l2:
+    .skip 4096, 0
+
+.align 12
+page_table_l3:
+    .skip 4096, 0
 
 exception_entry:
     stp     x20, x21, [sp, -16]!
@@ -90,16 +170,16 @@ exception_entry:
     mov     x0, sp
     bl      exception
 
-    ldp     x2, x3, [sp, #16]
-    ldp     x4, x5, [sp, #32]
-    ldp     x6, x7, [sp, #48]
-    ldp     x8, x9, [sp, #64]
-    ldp     x10, x11, [sp, #80]
-    ldp     x12, x13, [sp, #96]
-    ldp     x14, x15, [sp, #112]
-    ldp     x16, x17, [sp, #128]
-    ldp     x18, x29, [sp, #144]
-    ldp     x30, x0, [sp, #160]
+    ldp     x2, x3, [sp, 16]
+    ldp     x4, x5, [sp, 32]
+    ldp     x6, x7, [sp, 48]
+    ldp     x8, x9, [sp, 64]
+    ldp     x10, x11, [sp, 80]
+    ldp     x12, x13, [sp, 96]
+    ldp     x14, x15, [sp, 112]
+    ldp     x16, x17, [sp, 128]
+    ldp     x18, x29, [sp, 144]
+    ldp     x30, x0, [sp, 160]
     mov     x1, sp
     mov     sp, x0
     ldp     x0, x1, [x1, 0]
@@ -134,16 +214,16 @@ interrupt_entry:
     mov     x0, sp
     bl      interrupt
 
-    ldp     x2, x3, [sp, #16]
-    ldp     x4, x5, [sp, #32]
-    ldp     x6, x7, [sp, #48]
-    ldp     x8, x9, [sp, #64]
-    ldp     x10, x11, [sp, #80]
-    ldp     x12, x13, [sp, #96]
-    ldp     x14, x15, [sp, #112]
-    ldp     x16, x17, [sp, #128]
-    ldp     x18, x29, [sp, #144]
-    ldp     x30, x0, [sp, #160]
+    ldp     x2, x3, [sp, 16]
+    ldp     x4, x5, [sp, 32]
+    ldp     x6, x7, [sp, 48]
+    ldp     x8, x9, [sp, 64]
+    ldp     x10, x11, [sp, 80]
+    ldp     x12, x13, [sp, 96]
+    ldp     x14, x15, [sp, 112]
+    ldp     x16, x17, [sp, 128]
+    ldp     x18, x29, [sp, 144]
+    ldp     x30, x0, [sp, 160]
     mov     x1, sp
     mov     sp, x0
     ldp     x0, x1, [x1, 0]
