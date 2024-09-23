192b7cd0e30f5059876f69771554b28c90a44e5b
Dan Nasman <dan.nasman@gmail.com>
2024-09-23 20:02:39 +0300

    add panic print

--- tuoni/src/panic.rs
+++ tuoni/src/panic.rs
@@ -1,6 +1,9 @@
 use core::panic::PanicInfo;
 
+use crate::log_write;
+
 #[panic_handler]
-fn on_panic(_info: &PanicInfo) -> ! {
+fn on_panic(info: &PanicInfo) -> ! {
+    log_write!("KERNEL PANIC: {}\n", info);
     loop {}
 }
