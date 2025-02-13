5645de0428158155bc627be2f574e99c0f211c2b
Dan Nasman <dan.nasman@gmail.com>
2025-02-13 19:11:40 +0200

    fix formatting

--- tuoni/src/bsp/qemu/mmio.rs
+++ tuoni/src/bsp/qemu/mmio.rs
@@ -5,6 +5,40 @@ use crate::sync;
 
 static mut MMIO: sync::mutex::Mutex<Mmio> = sync::mutex::Mutex::new(Mmio::new());
 
+#[allow(non_camel_case_types)]
+pub enum Offset {
+    UART0_BASE = 0x0100_0000,
+    UART0_RSRECR = 0x0100_0000 + 0x04,
+    UART0_FR = 0x0100_0000 + 0x18,
+    UART0_ILPR = 0x0100_0000 + 0x20,
+    UART0_IBRD = 0x0100_0000 + 0x24,
+    UART0_FBRD = 0x0100_0000 + 0x28,
+    UART0_LCRH = 0x0100_0000 + 0x2c,
+    UART0_CR = 0x0100_0000 + 0x30,
+    UART0_IFLS = 0x0100_0000 + 0x34,
+    UART0_IMSC = 0x0100_0000 + 0x38,
+    UART0_RIS = 0x0100_0000 + 0x3c,
+    UART0_MIS = 0x0100_0000 + 0x40,
+    UART0_ICR = 0x0100_0000 + 0x44,
+    UART0_DMACR = 0x0100_0000 + 0x48,
+    UART0_ITCR = 0x0100_0000 + 0x80,
+    UART0_ITIP = 0x0100_0000 + 0x84,
+    UART0_ITOP = 0x0100_0000 + 0x88,
+    UART0_TDR = 0x0100_0000 + 0x8C,
+}
+
+impl Into<usize> for Offset {
+    fn into(self) -> usize {
+        self as usize
+    }
+}
+
+impl Into<isize> for Offset {
+    fn into(self) -> isize {
+        self as isize
+    }
+}
+
 #[derive(Debug)]
 pub struct Mmio {
     pub base: *mut u8,
@@ -49,39 +83,5 @@ pub fn delay(ticks: usize) {
         unsafe {
             asm!("nop");
         }
-    }
-}
-
-#[allow(non_camel_case_types)]
-pub enum Offset {
-    UART0_BASE = 0x0100_0000,
-    UART0_RSRECR = 0x0100_0000 + 0x04,
-    UART0_FR = 0x0100_0000 + 0x18,
-    UART0_ILPR = 0x0100_0000 + 0x20,
-    UART0_IBRD = 0x0100_0000 + 0x24,
-    UART0_FBRD = 0x0100_0000 + 0x28,
-    UART0_LCRH = 0x0100_0000 + 0x2c,
-    UART0_CR = 0x0100_0000 + 0x30,
-    UART0_IFLS = 0x0100_0000 + 0x34,
-    UART0_IMSC = 0x0100_0000 + 0x38,
-    UART0_RIS = 0x0100_0000 + 0x3c,
-    UART0_MIS = 0x0100_0000 + 0x40,
-    UART0_ICR = 0x0100_0000 + 0x44,
-    UART0_DMACR = 0x0100_0000 + 0x48,
-    UART0_ITCR = 0x0100_0000 + 0x80,
-    UART0_ITIP = 0x0100_0000 + 0x84,
-    UART0_ITOP = 0x0100_0000 + 0x88,
-    UART0_TDR = 0x0100_0000 + 0x8C,
-}
-
-impl Into<usize> for Offset {
-    fn into(self) -> usize {
-        self as usize
-    }
-}
-
-impl Into<isize> for Offset {
-    fn into(self) -> isize {
-        self as isize
     }
 }
--- tuoni/src/bsp/raspi4b/mmio.rs
+++ tuoni/src/bsp/raspi4b/mmio.rs
@@ -7,32 +7,32 @@ static mut MMIO: sync::mutex::Mutex<Mmio> = sync::mutex::Mutex::new(Mmio::new())
 
 #[allow(non_camel_case_types)]
 pub enum Offset {
-    GPIO_BASE       = 0x200000,
-    GPPUD           = 0x200000 + 0x94,
-    GPPUDCLK0       = 0x200000 + 0x98,
+    GPIO_BASE = 0x200000,
+    GPPUD = 0x200000 + 0x94,
+    GPPUDCLK0 = 0x200000 + 0x98,
 
-    UART0_BASE      = 0x201000,
-    UART0_RSRECR    = 0x201000 + 0x04,
-    UART0_FR        = 0x201000 + 0x18,
-    UART0_ILPR      = 0x201000 + 0x20,
-    UART0_IBRD      = 0x201000 + 0x24,
-    UART0_FBRD      = 0x201000 + 0x28,
-    UART0_LCRH      = 0x201000 + 0x2c,
-    UART0_CR        = 0x201000 + 0x30,
-    UART0_IFLS      = 0x201000 + 0x34,
-    UART0_IMSC      = 0x201000 + 0x38,
-    UART0_RIS       = 0x201000 + 0x3c,
-    UART0_MIS       = 0x201000 + 0x40,
-    UART0_ICR       = 0x201000 + 0x44,
-    UART0_DMACR     = 0x201000 + 0x48,
-    UART0_ITCR      = 0x201000 + 0x80,
-    UART0_ITIP      = 0x201000 + 0x84,
-    UART0_ITOP      = 0x201000 + 0x88,
-    UART0_TDR       = 0x201000 + 0x8C,
+    UART0_BASE = 0x201000,
+    UART0_RSRECR = 0x201000 + 0x04,
+    UART0_FR = 0x201000 + 0x18,
+    UART0_ILPR = 0x201000 + 0x20,
+    UART0_IBRD = 0x201000 + 0x24,
+    UART0_FBRD = 0x201000 + 0x28,
+    UART0_LCRH = 0x201000 + 0x2c,
+    UART0_CR = 0x201000 + 0x30,
+    UART0_IFLS = 0x201000 + 0x34,
+    UART0_IMSC = 0x201000 + 0x38,
+    UART0_RIS = 0x201000 + 0x3c,
+    UART0_MIS = 0x201000 + 0x40,
+    UART0_ICR = 0x201000 + 0x44,
+    UART0_DMACR = 0x201000 + 0x48,
+    UART0_ITCR = 0x201000 + 0x80,
+    UART0_ITIP = 0x201000 + 0x84,
+    UART0_ITOP = 0x201000 + 0x88,
+    UART0_TDR = 0x201000 + 0x8C,
 
-    MBOX_BASE       = 0xB880, // MBOX_READ
-    MBOX_STATUS     = 0xB880 + 0x18,
-    MBOX_WRITE      = 0xB880 + 0x20,
+    MBOX_BASE = 0xB880, // MBOX_READ
+    MBOX_STATUS = 0xB880 + 0x18,
+    MBOX_WRITE = 0xB880 + 0x20,
 }
 
 impl Into<usize> for Offset {
@@ -97,4 +97,3 @@ pub fn delay(ticks: usize) {
         }
     }
 }
-
